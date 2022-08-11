0014ad276b5f13cb3e4344656ad9c72a71b1ac53
KitaGaither <KitaGaither@users.noreply.github.com>
2022-08-11 21:27:13 +0000

    Lab challenge 1-forloops

---
+++ introfor/forloopchallenge1.py
@@ -0,0 +1,11 @@
+
+#!/usr/bin/env python3
+# CHALLENGE 01
+farms = [{"name": "NE Farm", "agriculture": ["sheep", "cows", "pigs", "chickens", "llamas", "cats"]},
+         {"name": "W Farm", "agriculture": ["pigs", "chickens", "llamas"]},
+         {"name": "SE Farm", "agriculture": ["chickens", "carrots", "celery"]}]
+for x in farms:
+    print("\nThe names of the farms are " + x, end="")
+print("\nThat's it folks.")
+
+# Need Help with this one because wtf???
